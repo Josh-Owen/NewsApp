bb602aa67a505146f2937db19b2964895a8be400
Josh Owen <erial909@hotmail.com>
2022-06-28 13:35:09 +0100

    Implemented some application constants

--- newsrepository/src/main/java/com/joshowen/newsrepository/Constants.kt
+++ newsrepository/src/main/java/com/joshowen/newsrepository/Constants.kt
@@ -1,4 +1,22 @@
 package com.joshowen.newsrepository
 
 // Todo: Put the API key somewhere better
-const val API_KEY = "69578afa477d494c8c66059d1d8100bf"
+//const val API_KEY = "69578afa477d494c8c66059d1d8100bf"
+
+//const val API_KEY = "347e31688c57427191f2e16e26fd63db"
+
+//const val API_KEY ="e54fd13cadeb4e88bb34468574ba0f1c"
+
+//const val API_KEY ="3b1cdaf445284e9588afdd344f51c501"
+
+const val API_KEY ="6862c62742e545588570dcae4645e6bb"
+
+//const val API_KEY = "1a2f8afaa600453cb03c9c4cee8faa69"
+
+//const val API_KEY = "1eb713751ce34efc8b67e137f2a4c1e6"
+
+const val ARTICLE_SEARCH_QUERY = "stock"
+
+const val PAGINATION_PAGE_SIZE = 10
+
+const val STARTING_PAGE_INDEX = 1
